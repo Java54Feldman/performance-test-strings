3f28e4cb225d6709067346cd260245d8c1916d3f
Pavel Feldman <feldmanpk@gmail.com>
2024-03-26 22:56:56 +0200

    updates SymbolRemoveStandard with "replace" instead of "replaceAll"

--- src/telran/strings/SymbolsRemoveStandard.java
+++ src/telran/strings/SymbolsRemoveStandard.java
@@ -4,7 +4,8 @@ public class SymbolsRemoveStandard implements SymbolsRemove {
 
 	@Override
 	public String removeSymbol(String str, char symbol) {
-		return str.replaceAll("\\Q" + symbol + "\\E", "");
+//		return str.replaceAll("\\Q" + symbol + "\\E", "");
+		return str.replace(Character.toString(symbol), "");
 	}
 
 }
